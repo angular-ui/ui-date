cbf95bc2e3c7b0e7200c1f0eba5d41d184ce5e0d
Pete Bacon Darwin <pete@bacondarwin.com>
2013-03-13 09:59:07 +0000

    chore(tests): add test config so that you can run the unit tests
    Removes dependency on angular-module
    Adds devDependency on angular-mocks
    Adds test.conf.js with a basic test setup
    Renames dateSpec.js to date.spec.js (as this is nicer to read IMO!)

---
+++ test.conf.js
@@ -0,0 +1,13 @@
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'components/jquery/jquery.js',
+  'components/jquery-ui/ui/jquery-ui.custom.js',
+  'components/angular/angular.js',
+  'components/angular-mocks/angular-mocks.js',
+  'date.js',
+  'test/*.spec.js'
+];
+singleRun = true;
+autoWatch = true;
+browsers = [ 'Chrome' ];
