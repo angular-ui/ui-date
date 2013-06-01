21b7fc9a6ee33a340161dec8ad1eab265e8e5eea
Wesley Cho <wesley.cho@gmail.com>
2013-06-01 11:55:45 -0400

    Fixed test.conf.js

--- test/test.conf.js
+++ test/test.conf.js
@@ -3,7 +3,7 @@ files = [
   JASMINE,
   JASMINE_ADAPTER,
   'components/jquery/jquery.js',
-  'components/jquery-ui/ui/jquery-ui.custom.js',
+  'components/jquery-ui/ui/minified/jquery-ui.min.js',
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
   'src/date.js',
