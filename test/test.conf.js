8801fd15f5144817e993b79c607822201f0ab43e
timmipetit <timmipetit@gmail.com>
2014-05-08 15:48:54 +0200

    Fix jquery inclusion
    Jquery seems to be distributed differently now, so change the includes

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,7 +2,7 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'bower_components/jquery/jquery.js',
+  'bower_components/jquery/dist/jquery.js',
   'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
   'bower_components/angular/angular.js',
   'bower_components/angular-mocks/angular-mocks.js',
